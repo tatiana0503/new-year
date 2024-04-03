d3280799cfba4fd7c80191d0d812c1a15c169f1c
tatiana0503 <tzhamoitina@gmail.com>
2024-04-03 09:04:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+function newYearCountdown () {
+    const newYearDay = new Date('January 1, 2025, 00:00');
+    const now = new Date();
+    const diff = newYearDay - now;
+
+    const msInSecond = 1000;
+    const msInMinute = 60*1000;
+    const msInHour = 60*60*1000;
+    const msInDay = 24*60*60*1000;
+
+    const displayDay = Math.floor(diff/msInDay);
+    document.querySelector('.days').textContent = displayDay;
+
+    const displayHour = Math.floor((diff%msInDay)/msInHour);
+    document.querySelector('.hours').textContent = displayHour;
+
+    const displayMinute = Math.floor((diff%msInHour)/msInMinute);
+    document.querySelector('.minutes').textContent = displayMinute;
+
+    const displaySecond = Math.floor((diff%msInMinute)/msInSecond);
+    document.querySelector('.seconds').textContent = displaySecond;
+    
+    if (diff <= 0){
+        document.querySelector('.days').textContent = 0;
+        document.querySelector('.hours').textContent = 0;
+        document.querySelector('.minutes').textContent = 0;
+        document.querySelector('.seconds').textContent = 0;
+        clearInterval(timerID);
+        happyNewYear();
+    } 
+
+   
+}
+let timerID = setInterval(newYearCountdown, 1000);
+
+function happyNewYear(){
+    const heading = document.querySelector('h1');
+    heading.textContent = "Happy New Year!!!";
+    heading.classList.add('red');
+}
+
+const button = document.querySelector('#btn');
+const audio = document.querySelector('#myAudio');
+button.addEventListener('click', function(){
+ if(audio.paused){
+    audio.play();
+ }
+ else {
+    audio.pause();
+ }
+})
